bb10dabe54ec31c22880506493409a0eda3a7301
osmancoskun <osman.coskun95@gmail.com>
2022-11-16 03:03:07 +0300

    added counter

--- src/App.svelte
+++ src/App.svelte
@@ -1,57 +1,13 @@
 <script lang="ts">
-  import { Button, Input } from "./components/index";
-  let colors = [
-    "blue",
-    "dark",
-    "light",
-    "green",
-    "red",
-    "yellow",
-    "purple",
-    "orange",
-    "cyan",
-    "pink",
-  ];
-  let value = "";
-  $: console.log(value);
+  let count: number = 0;
 </script>
 
-<!--main>
-  <p class="text-teal-500">Tailwind is working</p>
-  <div>
-    {#each colors as color}
-      <Button {color}>Merhaba</Button>
-    {/each}
-  </div>
-  <hr style="width:100%;border:1px solid red" />
-  <div>
-    {#each colors as color}
-      <Button gradient {color}>Merhaba</Button>
-    {/each}
-  </div>
-  <hr style="width:100%;border:1px solid red" />
-
-  <div>
-    {#each colors as color}
-      <Button shadow {color}>Merhaba</Button>
-    {/each}
-  </div>
-</main-->
-<form action="">
-  <Input
-    id="qwe"
-    bind:value
-    label="deneme"
-    required
-    placeholder="placeholder test"
-  />
-  <Input
-    id="qwe"
-    bind:value
-    label="deneme"
-    required
-    circled
-    placeholder="placeholder test"
-  />
-  <button>qwe</button>
-</form>
+<main class="flex h-screen w-screen items-center justify-center flex-col">
+  <span class="mb-5 font-bold text-xl"> Svelte + Vite + Tailwind </span>
+  <button
+    on:click={() => count++}
+    class="border border-orange-900 py-2 px-6 bg-orange-500 text-white"
+  >
+    Click me {count}
+  </button>
+</main>
